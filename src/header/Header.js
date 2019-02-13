c3991057f8430ba73daecd7ff2a0cca69b450753
malgorzatamaz <malgorzatamaz@gmail.com>
2019-02-13 12:16:14 +0100

    Added files for header

---
+++ src/header/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import "./Header.sass";
+
+const Header = () => (
+  <header className="header">
+    <a href="/" className="header__link">
+      Selleo
+    </a>
+    <ul className="header__list">
+      <li>
+        <a className="header__list-link" href="#blog">
+          Blog
+        </a>
+      </li>
+      <li>
+        <a className="header__list-link" href="#guide">
+          Style Guide
+        </a>
+      </li>
+      <li>
+        <a className="header__list-link" href="#manuals">
+          Manuals
+        </a>
+      </li>
+      <li>
+        <a className="header__list-link" href="#pages">
+          Pages
+        </a>
+      </li>
+      <li>
+        <a className="header__list-link" href="#contacts">
+          Contacts
+        </a>
+      </li>
+    </ul>
+  </header>
+);
+
+export default Header;
