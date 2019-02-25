ce655678e3639f4c0e3ba5fab9aa5615b8441479
malgorzatamaz <malgorzatamaz@gmail.com>
2019-02-25 16:50:37 +0100

    testing prettier on commit

--- src/header/header.js
+++ src/header/header.js
@@ -1,40 +1,40 @@
-import React from "react";
+import React from 'react'
 
-import "./header.sass";
+import './header.sass'
 
 const Header = () => (
-  <header className="header">
-    <a href="/" className="header__link">
-      Selleo
-    </a>
-    <ul className="header__list">
-      <li>
-        <a className="header__list-link" href="#blog">
-          Blog
+    <header className="header">
+        <a href="/" className="header__link">
+            Selleo
         </a>
-      </li>
-      <li>
-        <a className="header__list-link" href="#guide">
-          Style Guide
-        </a>
-      </li>
-      <li>
-        <a className="header__list-link" href="#manuals">
-          Manuals
-        </a>
-      </li>
-      <li>
-        <a className="header__list-link" href="#pages">
-          Pages
-        </a>
-      </li>
-      <li>
-        <a className="header__list-link" href="#contacts">
-          Contacts
-        </a>
-      </li>
-    </ul>
-  </header>
-);
+        <ul className="header__list">
+            <li>
+                <a className="header__list-link" href="#blog">
+                    Blog
+                </a>
+            </li>
+            <li>
+                <a className="header__list-link" href="#guide">
+                    Style Guide
+                </a>
+            </li>
+            <li>
+                <a className="header__list-link" href="#manuals">
+                    Manuals
+                </a>
+            </li>
+            <li>
+                <a className="header__list-link" href="#pages">
+                    Pages
+                </a>
+            </li>
+            <li>
+                <a className="header__list-link" href="#contacts">
+                    Contact
+                </a>
+            </li>
+        </ul>
+    </header>
+)
 
-export default Header;
+export default Header
