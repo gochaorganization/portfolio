3c15ed4035e2cf9bb622245cbe58383847af1280
malgorzatamaz <malgorzatamaz@gmail.com>
2019-03-14 20:57:07 +0100

    Setup tests

--- src/app.test.jsx
+++ src/app.test.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { shallow, render } from 'enzyme'
+import Footer from './components/footer'
+// import ListLink from './components/footer/listlink'
 
-import App from './app'
+// it('renders without crashing', () => {
+//   const div = document.createElement('div')
+//   ReactDOM.render(<Footer />, div)
+//   ReactDOM.unmountComponentAtNode(div)
+// })
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+it('has links', () => {
+  const wrapper = shallow(<Footer />)
+  console.log(wrapper.debug())
+
+  expect(wrapper.find('.footer__list').exists()).toBe(true)
 })
