963654decf5b20d53de18751031b8dfcf3775a65
malgorzatamaz <malgorzatamaz@gmail.com>
2019-03-19 06:27:14 +0100

    gallery slider

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import Slider from 'react-animated-slider'
+import _ from 'lodash'
+import { connect } from 'react-redux'
+
+import portfolio from 'redux/reducers/portfolio'
+
+import 'react-animated-slider/build/horizontal.css'
 
 class Gallery extends Component {
   render() {
-    return <div>Gallery</div>
+    const { images } = this.props
+
+    return (
+      <Slider>
+        {_.map(images, image => (
+          <div key={image.id}>
+            <h5>{image.title}</h5>
+            <img alt="" src={image.src} />
+          </div>
+        ))}
+      </Slider>
+    )
   }
 }
 
-export default Gallery
+const mapStateToProps = () => portfolio
+export default connect(mapStateToProps)(Gallery)
